328ba159e148adf8bf97540d6b27ac392e4c7a37
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-09-23 15:39:03 -0500

    Implement creating employee feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-employee-app';
+  title = 'Angular 10 Spring Boot CRUD';
 }
