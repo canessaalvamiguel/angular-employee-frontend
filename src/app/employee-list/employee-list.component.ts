1e42d084a0170ddee606fb1ef4a2ffc3366708ae
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-09-23 22:43:45 -0500

    Implement delete employee

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -28,4 +28,13 @@ export class EmployeeListComponent implements OnInit {
     this.router.navigate(['update-employee', id]);
   }
 
+  deleteEmployee(id: number){
+    this.employeeService.deleteEmployee(id).subscribe(data => {
+      console.log(data);
+      this.getEmployees();
+    },
+    error => console.error(error)
+    );
+  }
+
 }
